28b7caee4882a61718e2b8ba649adb366f88445f
kata-n <kata.kawa813@gmail.com>
2020-04-07 07:20:39 +0900

    ファビコン設定

--- resources/views/layouts/contents-template.blade.php
+++ resources/views/layouts/contents-template.blade.php
@@ -5,6 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta name="twitter:card" content="summary"/>
